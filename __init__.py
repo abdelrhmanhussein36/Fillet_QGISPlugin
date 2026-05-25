83e21273b1b212e3522d118f48f5e7c80452ec0a
Abdelrahman Hussein <abdelrhmanhussein36@gmail.com>
2026-05-26 01:18:10 +0300

    __init__.py

--- __init__.py
+++ __init__.py
@@ -1,34 +1,44 @@
-from qgis.PyQt.QtWidgets import QAction, QMessageBox
+import os
+from qgis.PyQt.QtWidgets import QAction
+from qgis.PyQt.QtGui import QIcon
+from qgis.core import Qgis, QgsApplication
+
 
 def classFactory(iface):
-    """Required entry point - this MUST exist"""
     return FilletPlugin(iface)
 
 
 class FilletPlugin:
     def __init__(self, iface):
         self.iface = iface
-        self.name = "Fillet Tool"
+        self.plugin_dir = os.path.dirname(__file__)
+        self.action = None
 
     def initGui(self):
-        """Create the menu entry and toolbar icon"""
-        # Create action without icon to avoid missing file errors
-        self.action = QAction("Fillet Tool", self.iface.mainWindow())
-        self.action.setWhatsThis("Fillet polygon corners")
+        # Use a default icon if custom icon doesn't exist
+        icon_path = os.path.join(self.plugin_dir, "icon.png")
+        if os.path.exists(icon_path):
+            icon = QIcon(icon_path)
+        else:
+            # Use built-in QGIS icon as fallback
+            icon = QgsApplication.getThemeIcon("/mActionFileSave.svg")
+        
+        self.action = QAction(icon, "Fillet Tool", self.iface.mainWindow())
+        self.action.setWhatsThis("Fillet polygon corners – ArcGIS Pro style")
+        self.action.setObjectName("FilletToolAction")  # Important for persistence
         self.action.triggered.connect(self.run)
         
-        # Add to toolbar and menu
         self.iface.addToolBarIcon(self.action)
         self.iface.addPluginToMenu("&Fillet Tool", self.action)
 
     def unload(self):
-        """Remove plugin from interface"""
-        self.iface.removePluginMenu("&Fillet Tool", self.action)
-        self.iface.removeToolBarIcon(self.action)
+        if self.action:
+            self.iface.removePluginMenu("&Fillet Tool", self.action)
+            self.iface.removeToolBarIcon(self.action)
+            self.action = None
 
     def run(self):
-        """Show a simple message to test if plugin loads"""
-        from .simple_dialog import SimpleFilletDialog
-        dialog = SimpleFilletDialog(self.iface)
+        from .simple_dialog import ArcGISProFilletDialog
+        dialog = ArcGISProFilletDialog(self.iface)
         dialog.show()
-        dialog.exec_()
+        dialog.exec()
